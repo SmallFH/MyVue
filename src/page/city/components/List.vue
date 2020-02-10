491a7dcc084b92ecb25fc69f456f9de08f2256e2
SmallFH <fu_hao_email@163.com>
2020-02-10 17:21:17 +0800

    add city-list todo

---
+++ src/page/city/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="list">
+      <div class="area">
+        当前城市
+      </div>
+      <div class="area">热门城市</div>
+      <div class="area">A</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CityList"
+    }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
